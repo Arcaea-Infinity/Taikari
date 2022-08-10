1d9f0f93b60ac9a53e24c97193e9680edd58a67a
t404owo <tags4owo@gmail.com>
2022-08-10 18:52:31 +0200

    Update taikari.js

--- taikari.js
+++ taikari.js
@@ -131,7 +131,7 @@ const config = {
       //{ name: 'libcocos2dcpp.so!OnlineManager::sendHttpRequest', proc:  },
       //{ name: 'libcocos2dcpp.so!OnlineManager::setFavoriteCharacter', proc:  }
     ],
-    '4.0.255c_1060002_armeabi-v7a' : [ //
+    '4.0.255c_1060002_armeabi-v7a' : [ // wip
       { name: 'libcocos2dcpp.so!curl_easy_setopt', proc: 0x3700b4 },
       { name: 'libcocos2dcpp.so!easy_perform', proc: 0x7c5e88 },
      // { name: 'libcocos2dcpp.so!OnlineManager::OnlineManager', proc: 0x4bd7e9 }, 
@@ -151,6 +151,13 @@ const config = {
      // { name: 'libcocos2dcpp.so!OnlineManager::OnlineManager', proc:  }, 
      // { name: 'libcocos2dcpp.so!OnlineManager::sendHttpRequest', proc:  },
      // { name: 'libcocos2dcpp.so!OnlineManager::setFavoriteCharacter', proc: 0x86e601 } 
+    ],
+    '4.0.256c_1070001_arm64-v8a' : [ //wip
+      { name: 'libcocos2dcpp.so!curl_easy_setopt', proc: 0xc81d44 },
+      { name: 'libcocos2dcpp.so!easy_perform', proc: 0xb30a88 },
+     // { name: 'libcocos2dcpp.so!OnlineManager::OnlineManager', proc:  }, 
+     // { name: 'libcocos2dcpp.so!OnlineManager::sendHttpRequest', proc:  },
+     // { name: 'libcocos2dcpp.so!OnlineManager::setFavoriteCharacter', proc:  } 
     ],
   }
 };
